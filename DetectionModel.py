df35c1111a2955094c7fb5e3c4237aa6661673e8
1exedra <exedreye@gmail.com>
2024-06-24 15:17:27 +0300

    Update DetectionModel.py
    Data preprocessing

--- DetectionModel.py
+++ DetectionModel.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+data = pd.read_csv('creditcard.csv')
+
+X = data.drop(['Class'], axis=1)
+y = data['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
