71a001b2918137dbde605716f5a5b5530fda391b
1exedra <exedreye@gmail.com>
2024-06-25 17:01:39 +0300

    Update DetectionModel.py
    Measuring the model's ability to distinguish between classes via ROC

--- DetectionModel.py
+++ DetectionModel.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, roc_auc_score
 data = pd.read_csv('creditcard.csv')
 
 X = data.drop(['Class'], axis=1)
@@ -12,3 +13,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train_scaled, y_train)
+
+y_pred = model.predict(X_test_scaled)
+y_pred_prob = model.predict_proba(X_test_scaled)[:, 1]
+
+print(classification_report(y_test, y_pred))
+print("ROC AUC Score:", roc_auc_score(y_test, y_pred_prob))
