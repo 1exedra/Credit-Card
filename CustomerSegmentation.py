d03d45a86dcb578da97eedab6b3f76d0f5cadf21
1exedra <exedreye@gmail.com>
2024-06-26 09:59:39 +0300

    Update CustomerSegmentation.py
    missing values and feature scaling

--- CustomerSegmentation.py
+++ CustomerSegmentation.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv('creditcard.csv')
+
+data.fillna(method='ffill', inplace=True)
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(data)
 
