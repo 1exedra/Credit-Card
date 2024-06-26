b9f483879d5531ad10b832a978cc47f3df4772b6
1exedra <exedreye@gmail.com>
2024-06-26 10:13:28 +0300

    Update CustomerSegmentation.py

--- CustomerSegmentation.py
+++ CustomerSegmentation.py
@@ -8,3 +8,16 @@ data.fillna(method='ffill', inplace=True)
 scaler = StandardScaler()
 scaled_features = scaler.fit_transform(data)
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+print(data.describe())
+
+for column in data.columns:
+    plt.figure(figsize=(10, 4))
+    sns.histplot(data[column], kde=True)
+    plt.title(f'Distribution of {column}')
+    plt.show()
+
+corr_matrix = data.corr()
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+plt.show()
