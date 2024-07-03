a55e4a677b2b6609ac0525342e7a1568b811d931
1exedra <exedreye@gmail.com>
2024-07-03 13:33:51 +0530

    Update CustomerLifetimeValue.py
    defining target variables and handling missing values

--- CustomerLifetimeValue.py
+++ CustomerLifetimeValue.py
@@ -1,1 +1,11 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df.fillna(0, inplace=True)  
+
+X = df.drop(['Amount', 'Class'], axis=1) 
+y = df['Amount']
+
+
 
