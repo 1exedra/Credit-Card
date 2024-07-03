2580192dc68ae0b6a7bb42fdea85b1cc230c4901
1exedra <exedreye@gmail.com>
2024-07-03 13:34:15 +0530

    Update CustomerLifetimeValue.py

--- CustomerLifetimeValue.py
+++ CustomerLifetimeValue.py
@@ -7,5 +7,8 @@ df.fillna(0, inplace=True)
 X = df.drop(['Amount', 'Class'], axis=1) 
 y = df['Amount']
 
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
